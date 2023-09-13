14c9d32589ac97a73d42044b19197c30cb98edfd
Lakshman <chendhurthilachababu@gmail.com>
2023-09-14 00:14:36 +0530

    inventory first commit

--- src/org/pvp/cse/poc/pizza/inventory/Inventory.java
+++ src/org/pvp/cse/poc/pizza/inventory/Inventory.java
@@ -1,31 +1,58 @@
 package org.pvp.cse.poc.pizza.inventory;
-
+import org.pvp.cse.poc.pizza.constants.SIZE;
 import org.pvp.cse.poc.pizza.model.Pizza;
 import org.pvp.cse.poc.pizza.model.Side;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Inventory {
-    Map inventory = new HashMap();
-
-    //stock availability check
-    //update stock
-    //Load Stock
+    Map<String,Map<SIZE,Integer>> pizzaInventory = new HashMap<>();
+    Map<String,Integer> toppingInventory = new HashMap<>();
+    Map<String,Integer> sideInventory = new HashMap<>();
+    //TODO:stock availability check
+    //TODO:update stock
+    //TODO:Load Stock
     Inventory(Pizza pizza, Side side){
 
     }
 
-    public boolean checkInventory(){
-        // check
-        return true;
+    public boolean checkPizzaInventory(Pizza pizza,SIZE size){
+
+        if (pizzaInventory.get(pizza).get(size)!=0)
+            return true;
+        else
+            return false;
+    }
+    public boolean checkToppingInventory(String topping){
+        if (toppingInventory.get(topping) != 0)
+            return true;
+        else
+            return false;
+    }
+    public boolean checkSideInventory(String side){
+        if (sideInventory.get(side) != 0)
+            return true;
+        else
+            return false;
     }
 
-    public void updateInventory(){
-      // to update ur inventiory
+    public void updatePizzaInventory(Pizza pizza,SIZE size){
+         //
+    }
+    public void updateToppingInventory(String topping) {
+        toppingInventory.put(topping,toppingInventory.get(topping)-1);
+    }
+    public void updateSideInventory(String side){
+        sideInventory.put(side,sideInventory.get(side)-1);
     }
 
-    public void loadInventory(){
-        // load inventiory
+    public void loadPizzaInventory(Pizza pizza,SIZE size){
+        //
+    }
+    public void loadToppingInventory(String topping,Integer qty){
+        toppingInventory.put(topping,toppingInventory.get(topping)+qty);
+    }
+    public void loadSideInventory(String side,Integer qty){
+        sideInventory.put(side,sideInventory.get(side)+qty);
     }
 }
