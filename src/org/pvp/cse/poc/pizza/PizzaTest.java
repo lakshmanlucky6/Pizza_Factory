b6010ee2e35597517a369442c31317d207415f34
teja <tejasreekatragadda11@gmail.com>
2023-09-12 20:07:52 -0700

    teja's first commit

--- src/org/pvp/cse/poc/pizza/PizzaTest.java
+++ src/org/pvp/cse/poc/pizza/PizzaTest.java
@@ -35,7 +35,7 @@ public class PizzaTest {
 
     private static void validateBusinessRules() {
         // Toppings
-        //TODO: tejasree
+        //TODO: tejasree;
     }
 
     private static void placeOrder() {
