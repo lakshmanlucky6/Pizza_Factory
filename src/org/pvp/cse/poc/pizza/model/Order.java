624cdeb6ce5e30a0045d0453c4d1cde2c30963ae
Lakshman <chendhurthilachababu@gmail.com>
2023-09-14 21:38:45 +0530

    inventory secon commit

--- src/org/pvp/cse/poc/pizza/model/Order.java
+++ src/org/pvp/cse/poc/pizza/model/Order.java
@@ -1,6 +1,13 @@
 package org.pvp.cse.poc.pizza.model;
 
+import java.util.List;
+
 public class Order {
     // List of Pizzas
     //List of Sides
+    //System.out.println("Welcome to PizzaFactory");
+    //System.out.println("Give your orders : ");
+    List<Pizza> list;
+    List<Side> side;
+    
 }
