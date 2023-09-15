9ce7e4e6b592677da4e83b76ba59d1dd087e34f2
Billa Deekshitha <billadeekshitha3@gmail.com>
2023-09-15 19:37:46 +0530

    deekshitha

--- src/org/pvp/cse/poc/pizza/PizzaTest.java
+++ src/org/pvp/cse/poc/pizza/PizzaTest.java
@@ -6,6 +6,10 @@ import org.pvp.cse.poc.pizza.inventory.Inventory;
 import org.pvp.cse.poc.pizza.model.Menu;
 import org.pvp.cse.poc.pizza.model.Order;
 import org.pvp.cse.poc.pizza.model.Pizza;
+import java.util.List;
+
+
+import javax.print.attribute.standard.Sides;
 import java.util.Scanner;//public void select(){
 
 public class PizzaTest {
@@ -41,13 +45,18 @@ public class PizzaTest {
         //TODO: BusinessRules deekshitha
     }
 
-    private static void placeOrder() {
+    private static void placeOrder(Map<Pizza>pizza, Map<Sides>side) {
         // thank you
         // Reference
         //TODO: deekshitha
-        System.out.println("Welcome Pizza Factory");
+        for(i:pizza.size()) {
+            if(!checkPizzaInventory(pizza.get(i), size, qty)) {
+                System.out.println(pizza+"not available");
+            }
+        }
         Order o=new Order();
-        o.cost();
+        System.out.println("BILL:"+o.cost());
+        System.out.println("Thank you");
     }
 
     private static void displayMenuTouser(Menu menu) {
@@ -57,57 +66,61 @@ public class PizzaTest {
     private static Order takeOrder() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Cook your PIZZERIA here!!");
-        //System.out.println("1.Veg\n2.Non-veg");
+        while(true){
+            //System.out.println("1.Veg\n2.Non-veg");
+            System.out.print("SELECT THE TYPE ?");
 
-        System.out.print("SELECT THE TYPE ?");
-        int input = sc.nextInt();
-        switch (input) {
-            case 1:
-                System.out.println("1.Delux pizza\n2.Cheese and Corn\n3.Panner Tikka");
-                System.out.print("SELECT ONE : ");
-                int in = sc.nextInt();
-                switch (in) {
-                    case 1:
-                        System.out.println("Regular: Rs. 150  Medium: Rs. 200  Large: Rs. 325");
-                        break;
-                    case 2:
-                        System.out.println("Regular: Rs. 175 Medium: Rs. 375 Large: Rs. 475");
-                        break;
-                    case 3:
-                        System.out.println("Regular: Rs. 160 Medium: Rs. 290 Large: Rs. 340");
-                        break;
-                }
-                System.out.println("SELECT THE SIZE ?");
-                System.out.println("HEY VEGGY !! SELECT THE CRUST");
-                System.out.println("1.New Hand Tossed\n2.Wheat thin crust\n3.Cheese Burst\n4.Fresh pan");
-                // System.out.println("HEY VEGGY !! SELECT THE CRUST");
-                int i = sc.nextInt();
-                System.out.println("CUSTOMIZE IT WITH YOUR OWN TOPPINGS");
-                System.out.println("1.Black olive (Rs 20) \n2.Capsicum (Rs 25)\n 3.Paneer (Rs 35)\n4.Mushroom (Rs 30)\n5.Fresh tomato (Rs 10)");
-                break;
+        }
+            System.out.print("SELECT THE TYPE ?");
+            int input = sc.nextInt();
+            switch (input) {
+                case 1:
+                    //System.out.println("1.Delux pizza\n2.Cheese and Corn\n3.Panner Tikka");
 
-            case 2:
-                System.out.println("1.Non-Veg Supreme\n2.Chicken Tikka\n3.Pepper Barbecue");
-                System.out.print("SELECT ONE");
-                int ind = sc.nextInt();
-                switch (ind) {
-                    case 1:
-                        System.out.println("Regular: Rs 190 Medium: Rs 325 Large: Rs 425");
-                        break;
-                    case 2:
-                        System.out.println("Regular: Rs 210 Medium: Rs 370 Large: Rs 500");
-                        break;
-                    case 3:
-                        System.out.println("Regular: Rs 220 Medium: Rs 380 Large: Rs 525");
-                        break;
-                }
-                System.out.println("HEY NON-VEGGY !! SELECT THE CRUST");
-                System.out.println("1.New Hand Tossed\n2.Wheat thin crust\n3.Cheese Burst\n4.Fresh pan");
-                //System.out.println("HEY NON-VEGGY !! SELECT THE CRUST");
-                int inx = sc.nextInt();
-                System.out.println("CUSTOMIZE IT WITH YOUR OWN TOPPINGS");
-                System.out.println("1.Chicken tikka (Rs 35)\n2.Barbeque chicken (Rs 45)\n3.Grilled chicken (Rs 40)");
-                break;
+                    System.out.print("SELECT ONE : ");
+                    int in = sc.nextInt();
+                    switch (in) {
+                        case 1:
+                            System.out.println("Regular: Rs. 150  Medium: Rs. 200  Large: Rs. 325");
+                            break;
+                        case 2:
+                            System.out.println("Regular: Rs. 175 Medium: Rs. 375 Large: Rs. 475");
+                            break;
+                        case 3:
+                            System.out.println("Regular: Rs. 160 Medium: Rs. 290 Large: Rs. 340");
+                            break;
+                    }
+                    System.out.println("SELECT THE SIZE ?");
+                    System.out.println("HEY VEGGY !! SELECT THE CRUST");
+                    System.out.println("1.New Hand Tossed\n2.Wheat thin crust\n3.Cheese Burst\n4.Fresh pan");
+                    // System.out.println("HEY VEGGY !! SELECT THE CRUST");
+                    int i = sc.nextInt();
+                    System.out.println("CUSTOMIZE IT WITH YOUR OWN TOPPINGS");
+                    System.out.println("1.Black olive (Rs 20) \n2.Capsicum (Rs 25)\n 3.Paneer (Rs 35)\n4.Mushroom (Rs 30)\n5.Fresh tomato (Rs 10)");
+                    break;
+
+                case 2:
+                    System.out.println("1.Non-Veg Supreme\n2.Chicken Tikka\n3.Pepper Barbecue");
+                    System.out.print("SELECT ONE");
+                    int ind = sc.nextInt();
+                    switch (ind) {
+                        case 1:
+                            System.out.println("Regular: Rs 190 Medium: Rs 325 Large: Rs 425");
+                            break;
+                        case 2:
+                            System.out.println("Regular: Rs 210 Medium: Rs 370 Large: Rs 500");
+                            break;
+                        case 3:
+                            System.out.println("Regular: Rs 220 Medium: Rs 380 Large: Rs 525");
+                            break;
+                    }
+                    System.out.println("HEY NON-VEGGY !! SELECT THE CRUST");
+                    System.out.println("1.New Hand Tossed\n2.Wheat thin crust\n3.Cheese Burst\n4.Fresh pan");
+                    //System.out.println("HEY NON-VEGGY !! SELECT THE CRUST");
+                    int inx = sc.nextInt();
+                    System.out.println("CUSTOMIZE IT WITH YOUR OWN TOPPINGS");
+                    System.out.println("1.Chicken tikka (Rs 35)\n2.Barbeque chicken (Rs 45)\n3.Grilled chicken (Rs 40)");
+                    break;
 
         }
         System.out.println("Need Extra cheese : ");
