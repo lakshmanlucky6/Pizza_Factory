a08ef2c86397350dc7ee28ce7f1d7003770dc44d
balakrishna.billa <balakrishna.billa@gmail.com>
2023-09-17 15:47:35 +0530

    validators

---
+++ src/org/pvp/cse/poc/pizza/validator/PizzaValidator.java
@@ -0,0 +1,14 @@
+package org.pvp.cse.poc.pizza.validator;
+
+import org.pvp.cse.poc.pizza.model.Pizza;
+
+public class PizzaValidator<PIzz> implements IValidator<Pizza> {
+  @Override
+  public void validate(Pizza pizza) throws Exception {
+
+  }
+
+  public void validateTopping() {
+
+  }
+}
---
+++ src/org/pvp/cse/poc/pizza/validator/SidesValidator.java
@@ -0,0 +1,10 @@
+package org.pvp.cse.poc.pizza.validator;
+
+import org.pvp.cse.poc.pizza.model.Side;
+
+public class SidesValidator implements IValidator<Side> {
+    @Override
+    public void validate(Side side) throws Exception {
+
+    }
+}
